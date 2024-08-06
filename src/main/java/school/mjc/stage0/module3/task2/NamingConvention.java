1d2abe7b180e24e51c93d3195b7f5dbd3154a87f
CtaRik <1602211@mail.ru>
2024-08-06 19:18:39 +0600

    task has done

--- src/main/java/school/mjc/stage0/module3/task2/NamingConvention.java
+++ src/main/java/school/mjc/stage0/module3/task2/NamingConvention.java
@@ -6,7 +6,7 @@ public class NamingConvention {
         int variable_one = 1;
         int variableTwo = 2;
         int variable_three = 3;
-        final int VARIABLE_FOUR = 4;
+        final int VARIABLEFOUR = 4;
         final int VARIABLE_FIVE = 5;
         int variableSix = 6;
         int variableSeven = 7;
