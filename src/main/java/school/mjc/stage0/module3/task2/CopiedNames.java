8831b67c7f4b4233f45e234041a61756d55a0737
CtaRik <1602211@mail.ru>
2024-08-06 20:28:21 +0600

    task has done

--- src/main/java/school/mjc/stage0/module3/task2/CopiedNames.java
+++ src/main/java/school/mjc/stage0/module3/task2/CopiedNames.java
@@ -3,16 +3,16 @@ package school.mjc.stage0.module3.task2;
 public class CopiedNames {
     public static void main(String[] args) {
         int variable = 0;
-        int variable2 = 2000;
+        int variableTwo = 2000;
         int value = 1000;
-        int variable3 = 4000;
-        int value2 = 3000;
+        int variableThree = 4000;
+        int valueTwo = 3000;
         int variables = 5000;
-        int variables2 = 7000;
-        int value3 = 90000;
+        int variablesTwo = 7000;
+        int valueThree = 90000;
         int number = 100000;
         int myInteger = 1000000;
         int myVar = 1234567;
-        int my____integer = 1234567;
+        int myNumber = 1234567;
     }
 }
